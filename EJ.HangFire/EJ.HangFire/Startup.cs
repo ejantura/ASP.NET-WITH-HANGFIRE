3ed55d567e34fe1de765f5a6e23be1fffa74b15e
NOWY\EWach <ewach@ilc.pl>
2018-03-13 14:44:03 +0100

    Add HangFire package

---
+++ EJ.HangFire/EJ.HangFire/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Hangfire;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(EJ.HangFire.Startup))]
+
+namespace EJ.HangFire
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalConfiguration.Configuration.UseSqlServerStorage("HangFireDbConnection");
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+        }
+    }
+}
